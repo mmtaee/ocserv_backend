21170a29696350e9a13ecbf40acfd23c76859aa2
Masoud <mmtaee64@gmail.com>
2024-06-21 18:39:49 +0330

    update ocserv user stats

--- ocserv/gql/stats/query.py
+++ ocserv/gql/stats/query.py
@@ -1,9 +1,11 @@
 import graphene
+from django.db.models import FloatField, Sum
+from django.db.models.functions import Coalesce
 from graphql_jwt.decorators import login_required
 
 from backend.internal.generics import PaginationInputType
 from backend.internal.utils import pagination
-from ocserv.gql.stats.types import TrafficStatsListType
+from ocserv.gql.stats.types import StatDateType, TrafficStatsListType
 from ocserv.models import MonthlyTrafficStat
 
 
@@ -11,16 +13,32 @@ class TrafficStatsQuery(graphene.ObjectType):
     ocserv_users_stats = graphene.Field(
         TrafficStatsListType,
         page_data=graphene.Argument(PaginationInputType, required=True),
+        date=graphene.Argument(StatDateType, required=False),
         ocserv_user_id=graphene.ID(required=False),
     )
 
     @staticmethod
     @login_required
     def resolve_ocserv_users_stats(
-        root, info, page_data: PaginationInputType, ocserv_user_id: id = None
+        root,
+        info,
+        page_data: PaginationInputType,
+        ocserv_user_id: id = None,
+        date: StatDateType = None,
     ) -> TrafficStatsListType:
         queryset = MonthlyTrafficStat.objects.all()
         if ocserv_user_id is not None:
             queryset = queryset.filter(user_id=ocserv_user_id)
+        if date is not None:
+            queryset = queryset.filter(month=date.month, year=date.year)
         new_queryset, pagination_detail = pagination(queryset, page_data)
-        return TrafficStatsListType(statistics=new_queryset, pagination=pagination_detail)
+        total = new_queryset.aggregate(
+            total_rx=Coalesce(Sum("rx"), 0, output_field=FloatField()),
+            total_tx=Coalesce(Sum("tx"), 0, output_field=FloatField()),
+        )
+        return TrafficStatsListType(
+            statistics=new_queryset,
+            pagination=pagination_detail,
+            total_rx=total["total_rx"],
+            total_tx=total["total_tx"],
+        )
--- ocserv/gql/stats/types.py
+++ ocserv/gql/stats/types.py
@@ -14,4 +14,11 @@ class TrafficStatsType(DjangoObjectType):
 
 class TrafficStatsListType(graphene.ObjectType):
     statistics = graphene.List(TrafficStatsType)
+    total_rx = graphene.Float(default_value=0, description="Total rx Traffic Stats")
+    total_tx = graphene.Float(default_value=0, description="Total tx Traffic Stats")
     pagination = graphene.Field(PaginationResponseType)
+
+
+class StatDateType(graphene.InputObjectType):
+    month = graphene.Int()
+    year = graphene.Int()
